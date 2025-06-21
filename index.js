f6b0de84ae4dce903ee47975a9e794eaea636bf6
Stercy Mutuku <mutukustercy93@gmail.com>
2025-06-21 08:23:03 +0300

    Sets up an event on DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded");
+    //targeting the specific paragraph with the id "text"
+    const paragraph = document.getElementById("text")
+    //the paragraph with text id is now changed to the new content or a new paragragh
+    paragraph.textContent = "This is really cool!"
+    //prints it out on the console to check if the paragraph contnt has indeed changed
+    console.log(paragraph);
+});
+//this console is important as it is consoled before the DOM has been loaded
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+
+
+
+
